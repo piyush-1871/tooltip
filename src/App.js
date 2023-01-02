c2df0b598bd01b8a27f5386d499325e6a3a60aa9
piyush-1871 <piyush1871b@gmail.com>
2023-01-02 16:08:19 +0530

    comments

--- src/App.js
+++ src/App.js
@@ -1,34 +1,72 @@
 import "./App.css";
+
+// imports from TippyJs
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
-import 'tippy.js/animations/scale.css';
-import 'tippy.js/themes/light.css';
+import "tippy.js/animations/scale.css";
+import "tippy.js/themes/light.css";
 
 function App() {
   return (
     <div className="App">
+      {/* Header */}
       <h1>Welcome to tooltip react app.</h1>
+      {/* Guide */}
       <h2>Hover to see the tooltip.</h2>
 
+      {/* Container */}
       <div className="wrapper">
         <div>
-          <Tippy theme="dark" animation="scale" content="Hey I am the Left Tooltip" placement="left">
+          {/* Tippy Component */}
+          <Tippy
+            theme="dark"
+            animation="scale"
+            content="Hey I am the Left Tooltip"
+            placement="left"
+          >
+            {/* for left tooltip */}
+
             <span className="span">Hover Me</span>
           </Tippy>
         </div>
         <div>
-          <Tippy theme="dark" animation="scale" content="Hey I am the Top Tooltip" placement="top">
-            <span className="span">Hover Me</span>
-          </Tippy>
-        </div>
-        <div>
-          <Tippy theme="dark" animation="scale" content="Hey I am the Bottom Tootip" placement="bottom">
+          {/* Tippy Component */}
+
+          <Tippy
+            theme="dark"
+            animation="scale"
+            content="Hey I am the Top Tooltip"
+            placement="top"
+          >
+            {/* for top tooltip */}
+
             <span className="span">Hover Me</span>
           </Tippy>
         </div>
 
         <div>
-          <Tippy theme="dark" animation="scale" content="Hey I am the Right Tooltip" placement="right">
+          {/* Tippy Component */}
+          <Tippy
+            theme="dark"
+            animation="scale"
+            content="Hey I am the Bottom Tootip"
+            placement="bottom"
+          >
+            {/* for bottom tooltip */}
+            <span className="span">Hover Me</span>
+          </Tippy>
+        </div>
+
+        <div>
+          {/* Tippy Component */}
+          <Tippy
+            theme="dark"
+            animation="scale"
+            content="Hey I am the Right Tooltip"
+            placement="right"
+          >
+            {/* for right tooltip */}
+
             <span className="span">Hover Me</span>
           </Tippy>
         </div>
